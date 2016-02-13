d30011994a9f965b18f065b87bff18019d385afd
Garrett Mace <garrett@macetech.com>
2016-02-13 15:05:16 -0800

    Fix some tabbing

--- mtExtraButtons.c
+++ mtExtraButtons.c
@@ -10,18 +10,19 @@
 
 #include <ApplicationServices/ApplicationServices.h>
 
-CGEventRef myCGEventCallback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon) {
+CGEventRef myCGEventCallback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon)
+{
 
 	// Pass the event through if it's somehow not what we're looking for
-    if (type != kCGEventOtherMouseDown) return event;
+	if (type != kCGEventOtherMouseDown) return event;
+
+	// determine which mouse button was pressed    
+	int mouseButton = CGEventGetIntegerValueField(event, kCGMouseEventButtonNumber);
     
-    // determine which mouse button was pressed    
-    int mouseButton = CGEventGetIntegerValueField(event, kCGMouseEventButtonNumber);
-    
-    // left and right buttons are 0 and 1, middle is 2
-    // middle works as usual in most apps but is still an OtherMouseDown event
-    // only trigger on buttons 3 and 4
-    if (mouseButton == 3 || mouseButton == 4) {
+	// left and right buttons are 0 and 1, middle is 2
+	// middle works as usual in most apps but is still an OtherMouseDown event
+	// only trigger on buttons 3 and 4
+	if (mouseButton == 3 || mouseButton == 4) {
     
 		CGEventRef keyPressEvent = NULL;
 	
@@ -53,9 +54,9 @@ CGEventRef myCGEventCallback(CGEventTapProxy proxy, CGEventType type, CGEventRef
 		
 		CFRelease(keyPressEvent);
 
-		// if we return the event, it'll be passed through as usual to the UI
-		// extra button clicks still register as clicks on OSX, causing problems
-		// so we return NULL to eat these events
+	// if we return the event, it'll be passed through as usual to the UI
+	// extra button clicks still register as clicks on OSX, causing problems
+	// so we return NULL to eat these events
         return NULL;
         
     } else {
@@ -68,33 +69,33 @@ CGEventRef myCGEventCallback(CGEventTapProxy proxy, CGEventType type, CGEventRef
 
 int main(void)
 {
-    CFMachPortRef      eventTap;
-    CGEventMask        eventMask;
-    CFRunLoopSourceRef runLoopSource;
-
-    // Create an event tap.
-    eventMask = (1 << kCGEventOtherMouseDown);
-    eventTap = CGEventTapCreate(kCGHIDEventTap, kCGHeadInsertEventTap, 0, eventMask, myCGEventCallback, NULL);
-                   
-    if (!eventTap) {
-        fprintf(stderr, "Failed to create event tap\n");
-        exit(1);
-    }
-
-    // Create a run loop source
-    runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0);
-
-    // Add to the current run loop
-    CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, kCFRunLoopCommonModes);
-
-    // Enable the event tap
-    CGEventTapEnable(eventTap, true);
-
-    // Start the run loop
-    CFRunLoopRun();
-
-    // Attempt some cleanup
-    CFRunLoopRemoveSource(CFRunLoopGetCurrent(), runLoopSource, kCFRunLoopCommonModes);
-    CFRelease(runLoopSource);
-    exit(0);
+	CFMachPortRef      eventTap;
+	CGEventMask        eventMask;
+	CFRunLoopSourceRef runLoopSource;
+	
+	// Create an event tap.
+	eventMask = (1 << kCGEventOtherMouseDown);
+	eventTap = CGEventTapCreate(kCGHIDEventTap, kCGHeadInsertEventTap, 0, eventMask, myCGEventCallback, NULL);
+	           
+	if (!eventTap) {
+	fprintf(stderr, "Failed to create event tap\n");
+	exit(1);
+	}
+	
+	// Create a run loop source
+	runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0);
+	
+	// Add to the current run loop
+	CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, kCFRunLoopCommonModes);
+	
+	// Enable the event tap
+	CGEventTapEnable(eventTap, true);
+	
+	// Start the run loop
+	CFRunLoopRun();
+	
+	// Attempt some cleanup
+	CFRunLoopRemoveSource(CFRunLoopGetCurrent(), runLoopSource, kCFRunLoopCommonModes);
+	CFRelease(runLoopSource);
+	exit(0);
 }
