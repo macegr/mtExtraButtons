687a2fbe69a3eeb2830e6d891330046911bd5d13
macegr <garrett@macetech.com>
2016-11-25 12:45:37 -0800

    Added line-per-click scroll wheel acceleration

--- mtExtraButtons.c
+++ mtExtraButtons.c
@@ -6,64 +6,81 @@
 // Garrett Mace Feb 2016
 // Heavily modified from altermouse.c on http://osxbook.com
 //
+// Scroll wheel event tap adapted from DiscreteScroll by Emre Yolcu
+//
 // Compile with: gcc -Wall -o mtExtraButtons mtExtraButtons.c -framework ApplicationServices
 
 #include <ApplicationServices/ApplicationServices.h>
 
+const int LINES = 5;
+
 CGEventRef myCGEventCallback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon) {
 
-	// Pass the event through if it's somehow not what we're looking for
-    if (type != kCGEventOtherMouseDown) return event;
-    
-    // determine which mouse button was pressed    
-    int mouseButton = CGEventGetIntegerValueField(event, kCGMouseEventButtonNumber);
-    
-    // left and right buttons are 0 and 1, middle is 2
-    // middle works as usual in most apps but is still an OtherMouseDown event
-    // only trigger on buttons 3 and 4
-    if (mouseButton == 3 || mouseButton == 4) {
-    
-		CGEventRef keyPressEvent = NULL;
-	
-		if (mouseButton == 3) {
-			
-			// send Command-[ key down
-			keyPressEvent = CGEventCreateKeyboardEvent (NULL, (CGKeyCode)0x21, true);
-			CGEventSetFlags(keyPressEvent,kCGEventFlagMaskCommand);
-			CGEventPost(kCGHIDEventTap, keyPressEvent);
-			
-			// send Command-[ key up
-			keyPressEvent = CGEventCreateKeyboardEvent (NULL, (CGKeyCode)0x21, false);
-			CGEventSetFlags(keyPressEvent,kCGEventFlagMaskCommand);
-			CGEventPost(kCGHIDEventTap, keyPressEvent);
+	if (type == kCGEventOtherMouseDown){	// mouse buttons detected
+		
+		// determine which mouse button was pressed
+		int mouseButton = CGEventGetIntegerValueField(event, kCGMouseEventButtonNumber);
 
-		} else if (mouseButton == 4) {
+		// left and right buttons are 0 and 1, middle is 2
+		// middle works as usual in most apps but is still an OtherMouseDown event
+		// only trigger on buttons 3 and 4
+		if ((type == kCGEventOtherMouseDown) && (mouseButton == 3 || mouseButton == 4)) {
+
+			CGEventRef keyPressEvent = NULL;
+
+			if (mouseButton == 3) {
+				
+				// send Command-[ key down
+				keyPressEvent = CGEventCreateKeyboardEvent (NULL, (CGKeyCode)0x21, true);
+				CGEventSetFlags(keyPressEvent,kCGEventFlagMaskCommand);
+				CGEventPost(kCGHIDEventTap, keyPressEvent);
+				
+				// send Command-[ key up
+				keyPressEvent = CGEventCreateKeyboardEvent (NULL, (CGKeyCode)0x21, false);
+				CGEventSetFlags(keyPressEvent,kCGEventFlagMaskCommand);
+				CGEventPost(kCGHIDEventTap, keyPressEvent);
+
+			} else if (mouseButton == 4) {
+				
+				// send Command-] key down
+				keyPressEvent = CGEventCreateKeyboardEvent (NULL, (CGKeyCode)0x1E, true);
+				CGEventSetFlags(keyPressEvent,kCGEventFlagMaskCommand);
+				CGEventPost(kCGHIDEventTap, keyPressEvent);
+				
+				// send Command-] key up
+				keyPressEvent = CGEventCreateKeyboardEvent (NULL, (CGKeyCode)0x1E, false);
+				CGEventSetFlags(keyPressEvent,kCGEventFlagMaskCommand);
+				CGEventPost(kCGHIDEventTap, keyPressEvent);
+				
+			}
 			
-			// send Command-] key down
-			keyPressEvent = CGEventCreateKeyboardEvent (NULL, (CGKeyCode)0x1E, true);
-			CGEventSetFlags(keyPressEvent,kCGEventFlagMaskCommand);
-			CGEventPost(kCGHIDEventTap, keyPressEvent);
-			
-			// send Command-] key up
-			keyPressEvent = CGEventCreateKeyboardEvent (NULL, (CGKeyCode)0x1E, false);
-			CGEventSetFlags(keyPressEvent,kCGEventFlagMaskCommand);
-			CGEventPost(kCGHIDEventTap, keyPressEvent);
-			
+			CFRelease(keyPressEvent);
+
+			// if we return the event, it'll be passed through as usual to the UI
+			// extra button clicks still register as clicks on OSX, causing problems
+			// so we return NULL to eat these events
+			return NULL;
 		}
 		
-		CFRelease(keyPressEvent);
-
-		// if we return the event, it'll be passed through as usual to the UI
-		// extra button clicks still register as clicks on OSX, causing problems
-		// so we return NULL to eat these events
-        return NULL;
-        
-    } else {
+	} else if (type == kCGEventScrollWheel) {	// scroll wheel detected
+		
+		// The external scroll wheel seems to get pulled into the OS as a "pixel" per wheel click rather than a line. Yet when using
+		// an external wheel, the UI won't scroll with only one pixel per click. I have taken the pixel value and piped it to the line
+		// value with a max clamp. Each slow click will deliver one line scroll, and faster clicks will do a little acceleration up to
+		// the maximum value. It's not like acceleration was bad, just sucks to have zero movement on the first few scroll clicks.
+		if (!CGEventGetIntegerValueField(event, kCGScrollWheelEventIsContinuous)) {	// ignore continuous-scroll devices like the touchpad
+			int64_t delta = CGEventGetIntegerValueField(event, kCGScrollWheelEventPointDeltaAxis1);	// signed scroll wheel motion
+			fprintf(stderr, "Scroll delta %d", (int)delta);
+			if (delta > LINES) delta = LINES; if (delta < -LINES) delta = -LINES;	// clamp to fixed +/- increment
+			CGEventSetIntegerValueField(event,kCGScrollWheelEventDeltaAxis1, delta);	// edit the event fields
+			return event;	// pass the modified event through to the UI
+		}
+		
+	}
     	
-    	// if we detect other button presses just pass them through
-    	return event;
-    
-    }
+	// if we detect other button presses just pass them through
+	return event;
+		
 }
 
 int main(void)
@@ -73,8 +90,8 @@ int main(void)
     CFRunLoopSourceRef runLoopSource;
 
     // Create an event tap.
-    eventMask = (1 << kCGEventOtherMouseDown);
-    eventTap = CGEventTapCreate(kCGHIDEventTap, kCGHeadInsertEventTap, 0, eventMask, myCGEventCallback, NULL);
+    eventMask = (1 << kCGEventOtherMouseDown) | (1 << kCGEventScrollWheel);
+    eventTap = CGEventTapCreate(kCGSessionEventTap, kCGHeadInsertEventTap, 0, eventMask, myCGEventCallback, NULL);
                    
     if (!eventTap) {
         fprintf(stderr, "Failed to create event tap\n");
